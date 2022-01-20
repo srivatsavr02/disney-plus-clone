83695ea9fec47f877533d2ab9ba4b971e7daf8e7
srivatsavr02 <srivatsavr02@gmail.com>
2022-01-20 21:59:46 +0530

    login

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Link
 } from "react-router-dom";
 import Detail from './components/Detail'
+import Login from './components/Login'
 
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+        <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
