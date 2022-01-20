312cf7f2e2c2c0b8a40ddca32c96ed7d6b3a06c9
srivatsavr02 <srivatsavr02@gmail.com>
2022-01-20 21:15:59 +0530

    details complete

---
+++ src/components/Movies.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Movies() {
+  return (
+    <Container>
+        <h4>Recommended for You</h4>
+        <Content>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+            <Wrap>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/09/The-Simpsons-on-a-Swing.png" />
+            </Wrap>
+        </Content>
+    </Container>
+  );
+}
+
+export default Movies;
+
+const Container = styled.div`
+
+`
+
+const Content = styled.div`
+    display: grid;
+    grid-gap: 25px;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+`
+
+const Wrap = styled.div`
+    cursor: pointer;
+    border-radius: 10px;
+    overflow: hidden;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-brzier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    }
+`
